141c2ef385b07e2a550927ef714fb25a64f14e04
Harold Alcala <harold.alcala@vcube.co.jp>
2019-03-21 16:26:04 +0800

    Quiz Game
    UI Prep, Signaling, AgoraRtcEngine

--- src/components/GamePanel.js
+++ src/components/GamePanel.js
@@ -8,7 +8,7 @@ import PlayerPanel from './PlayerPanel';
 import QuestionPanel from './QuestionPanel';
 
 export default function(props) {
-    const [state, setState] = useState({_GAME_ID: 'p1-6DVWXJW'});
+    const [state, setState] = useState({_GAME_ID: 'G3y4WjinkP'});
 
     const {quiz_engine} = props;
     const {game_role, quiz_role, PLAYER_ID, game_status} = quiz_engine;
